05c83948fb22abe263a6ffa884ec1e9b9533c32f
Silvia Roldán <silviarf2902@gmail.com>
2021-11-01 12:08:29 +0100

    docs: :memo: Added comments

--- Práctica_2/Ejercicios/ejercicio2.c
+++ Práctica_2/Ejercicios/ejercicio2.c
@@ -6,24 +6,24 @@
 int nfile=0;
 
 void * nlineas(void * fichero){
-    int * nlineas=malloc(sizeof(int));
+    int * nlineas=malloc(sizeof(int)); //Se reserva memoria
     *nlineas=0;
 
-    FILE* f=fopen((char *) fichero,"r");
+    FILE* f=fopen((char *) fichero,"r"); //Se abren los ficheros pasados como argumento en modo lectura
     if(f==NULL){
         printf("ERROR: No se ha podido abrir el archivo\n");
         exit(EXIT_FAILURE);
     }
 
     char cadena[256];
-    while (fgets(cadena, 256, f)!=NULL){
+    while (fgets(cadena, 256, f)!=NULL){ //Se van leyendo cada una de las líneas del fichero
         ++*nlineas;
     }
     nfile++;
-    char * filename=(char *) fichero;
-    printf("Nº de líneas del fichero %dº (%s): %d\n",nfile,filename,*nlineas);
-    fclose(f);
-    pthread_exit((void *)nlineas);
+    char * filename=(char *) fichero; //Se realiza un casting al nombre del fichero pasado como argumento
+    printf("Nº de líneas del fichero %dº (%s): %d\n",nfile,filename,*nlineas); //Se imprimen las sumas parciales
+    fclose(f); //Se cierra el fichero
+    pthread_exit((void *)nlineas); //Se devuelve un puntero void* que apunta a la suma parcial de líneas del fichero leido
 }
 
 int main(int argc,char ** argv){
@@ -33,11 +33,11 @@ int main(int argc,char ** argv){
     }
 
     srand(time(NULL));
-    int N_hebras=argc-1;
+    int N_hebras=argc-1; //Número de hebras introducido por terminal
     pthread_t thr[N_hebras]; // Declaracion de un Array de hebras  
 
     for(int i=0;i<N_hebras;i++){
-        if(pthread_create(&(thr[i]), NULL, nlineas, (void*) argv[i+1])){
+        if(pthread_create(&(thr[i]), NULL, nlineas, (void*) argv[i+1])){ //Se crean N_hebras y se envían a la función nlineas() con cada uno de los ficheros
             fprintf(stderr, "Error creating thread\n");
 	        exit(EXIT_FAILURE);
         }
@@ -46,13 +46,13 @@ int main(int argc,char ** argv){
     int suma=0;
     int *ret;
 
-    for(int i=0;i<N_hebras;i++){
-        if(pthread_join(thr[i],(void**) &ret)){
+    for(int i=0;i<N_hebras;i++){ 
+        if(pthread_join(thr[i],(void**) &ret)){ //Se esperan a las N_hebras y se devuelve un puntero al resultado de las sumas parciales
             fprintf(stderr, "Error joining thread\n");
             exit(EXIT_FAILURE);  
         }
 
-        suma+=*ret;
+        suma+=*ret; //Se almacena en la variable suma la dirección del puntero ret que apunta al valor de cada una de las sumas parciales
     }
 
     printf("La suma de todos las líneas de los ficheros indicados es: %d\n",suma);
