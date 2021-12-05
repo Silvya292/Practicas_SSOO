cfcb6d64d8e4eaee8bb28eb3262374843870b6cd
Silvia Roldán <silviarf2902@gmail.com>
2021-12-05 17:56:29 +0100

    feat: :zap: Finished P3

--- Práctica_3/Ejercicios/ejercicio3b.c
+++ Práctica_3/Ejercicios/ejercicio3b.c
@@ -3,11 +3,14 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define tamBuff 5
+#define tamBuff 3
 
 sem_t s;
 sem_t cn;
 sem_t pr;
+
+int hebraProductor=0;
+int hebraConsumidor=0;
 
 int buffer[tamBuff];
 int indexin=0;
@@ -33,36 +36,31 @@ int extractDato(){
     return n;
 }
 
-void * productor(void * nIt){
-    int *nItproducer=(int *) nIt;
-    while(*nItproducer>0){
-        int n=(rand()%10)+1;
-        sem_wait(&pr);
-        sem_wait(&s);
-        addDato(n);
-        printf("→ Productor %ld: Insertado %d en el buffer\n",pthread_self(),n);
-        imprimeVector();
-        sem_post(&s);
-        sem_post(&cn);
-        --*nItproducer;
-    }
+void * productor(){
+    int n=(rand()%10)+1;
+    sem_wait(&pr);
+    sem_wait(&s);
+    hebraProductor++;
+    addDato(n);
+    printf("→ Productor %d (%ld): Insertado %d en el buffer\n",hebraProductor,pthread_self(),n);
+    imprimeVector();
+    sem_post(&s);
+    sem_post(&cn);
+    
     pthread_exit(NULL);
 }
 
-void * consumidor(void * nIt){
-    int *nItconsumer=(int *) nIt;
-    while(*nItconsumer>0){
-        sem_wait(&cn);
-        sem_wait(&s);
-        printf("← Consumidor %ld: Extraido %d del buffer\n",pthread_self(),extractDato());
-        imprimeVector();
-        sem_post(&s);
-        sem_post(&pr);
-        --*nItconsumer;
-    }
+void * consumidor(){
+    sem_wait(&cn);
+    sem_wait(&s);
+    hebraConsumidor++;
+    printf("← Consumidor %d (%ld): Extraido %d del buffer\n",hebraConsumidor,pthread_self(),extractDato());
+    imprimeVector();
+    sem_post(&s);
+    sem_post(&pr);
+
     pthread_exit(NULL);
 }
-
 
 int main(int argc, char **argv){
     sem_init(&s,0,1);
@@ -74,24 +72,23 @@ int main(int argc, char **argv){
     }
 
     srand(time(NULL));
-    pthread_t thr[2];
-    int nIt[2];
+    pthread_t thr[6];
 
-    for(int i=0;i<2;i++){
-        nIt[i]=10;
-    }
-
-    if(pthread_create(&(thr[0]), NULL,(void *) productor,(void *) &nIt[0])){
+    for(int i=0;i<3;i++){
+        if(pthread_create(&(thr[i]), NULL,(void *) productor, NULL)){
             fprintf(stderr, "Error creating thread\n");
 	        exit(EXIT_FAILURE);
+        }
     }
 
-    if(pthread_create(&(thr[1]), NULL,(void *) consumidor,(void *) &nIt[1])){
+    for(int i=3;i<6;i++){
+        if(pthread_create(&(thr[i]), NULL,(void *) consumidor, NULL)){
             fprintf(stderr, "Error creating thread\n");
 	        exit(EXIT_FAILURE);
+        }
     }
 
-    for(int i=0;i<2;i++){
+    for(int i=0;i<6;i++){
         if(pthread_join(thr[i],NULL)){
             fprintf(stderr, "Error joining thread\n");
             exit(EXIT_FAILURE);  
